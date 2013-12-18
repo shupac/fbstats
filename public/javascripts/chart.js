daa603684583604feb7caf837592974fa8b74c21
Hongxu Liu <djshu.us@gmail.com>
2013-12-18 10:53:01 -0800

    Separated out js to services.js

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -33,13 +33,14 @@ $(function () {
         },
         plotOptions: {
             area: {
-                fillColor: {
-                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
-                    stops: [
-                        [0, Highcharts.getOptions().colors[0]],
-                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                    ]
-                },
+                // fillColor: {
+                //     linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
+                //     stops: [
+                //         [0, Highcharts.getOptions().colors[0]],
+                //         [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                //     ]
+                // },
+                fillColor: 'blue',
                 lineWidth: 1,
                 marker: {
                     enabled: false
---
+++ public/javascripts/services.js
@@ -0,0 +1,90 @@
+angular.module('TwitterShopApp')
+.service('DateService', function($http) {
+  var increment = 5;
+  var startDate = new Date('Wed Dec 18 00:00:00 +0000 2013')
+  var service = {
+    scrubData: function(tweets) {
+      var data = [];
+      for(var i = 0; i < 100; i++) {
+        data.push(0);
+      }
+      for(var i = 0; i < tweets.length; i++) {
+        var count = 0;
+        var tweetDate = new Date(tweets[i].created);
+        var timeDiff = Math.floor((tweetDate - startDate)/1000/60); // mins
+        data[Math.floor(timeDiff/increment)-1]++;
+      }
+      return data;
+    }
+  };
+  return service;
+})
+.service('ChartService', function(DateService) {
+  var service = {
+    initialize: function(data) {
+      var factor = data.NYC.length / 80;
+
+      var circleSf = document.querySelectorAll('circle.sf')[0];
+      var circleChi = document.querySelectorAll('circle.chi')[0];
+      var circleNyc = document.querySelectorAll('circle.nyc')[0];
+
+      circleSf.setAttribute('r', data.SF.length/factor);
+      circleChi.setAttribute('r', data.CHI.length/factor);
+      circleNyc.setAttribute('r', data.NYC.length/factor);
+
+      circleSf.addEventListener('click', function() {
+        service.plot(DateService.scrubData(data.SF));
+      });
+
+      circleChi.addEventListener('click', function() {
+        service.plot(DateService.scrubData(data.CHI));
+      });
+
+      circleNyc.addEventListener('click', function() {
+        service.plot(DateService.scrubData(data.NYC));
+      });
+    },
+    plot: function(tweetData) {
+      $('#chart').highcharts({
+        chart: {
+          zoomType: 'x',
+          spacingRight: 0
+        },
+        title: {
+          text: 'Shopping Tweets'
+        },
+        // subtitle: {
+        //     text: document.ontouchstart === undefined ?
+        //         'Click and drag in the plot area to zoom in' :
+        //         'Pinch the chart to zoom in'
+        // },
+        xAxis: {
+          type: 'datetime',
+          maxZoom: 1 * 3600000,
+          title: {
+            text: 'Time'
+          }
+        },
+        yAxis: {
+          title: {
+            text: 'Number of Tweets'
+          }
+        },
+        tooltip: {
+          shared: true
+        },
+        legend: {
+          enabled: false
+        },
+        series: [{
+          type: 'column',
+          name: 'Number of Tweets',
+          pointInterval: 300 * 1000,
+          pointStart: Date.UTC(2013, 8, 18),
+          data: tweetData
+        }]
+      });
+    }
+  };
+  return service;
+});
